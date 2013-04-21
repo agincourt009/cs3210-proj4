5ae1fbee11e6cd3f5af392113e408b6fd0db1dba
agincourt009 <agincourt009@gmail.com>
2013-04-21 19:58:22 -0400

    Fixing the string conversion problem.

--- LoginPanel.java
+++ LoginPanel.java
@@ -64,20 +64,20 @@ public class LoginPanel extends JPanel
 				char[] temp= text.getPassword();
 				String pass = new String(temp);
 				String usern = user.getText();
-				
+				/**
 				Memory passmem = new Memory(pass.length());
 				passmem.write(0, pass.getBytes(), 0, pass.length());
 				
 				Memory usermem = new Memory(usern.length());
 				usermem.write(0, usern.getBytes(), 0, usern.length());
+				*/
 				
-				/**
 				Pointer usermem = new Memory(usern.length() + 1);
 				usermem.setString(0, usern);
 				
 				Pointer passmem = new Memory(pass.length() + 1);
 				passmem.setString(0, pass);
-				
+				/**
 				int passlength = Native.toCharArray(pass).length;
 				int userlength = Native.toCharArray(usern).length;
 				*/
